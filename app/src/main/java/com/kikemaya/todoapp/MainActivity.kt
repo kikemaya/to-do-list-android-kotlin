33328ae151cfdd31e8a88f357a8f2a5ea53667d9
kikemaya <enrique.mg.cd@gmail.com>
2023-06-28 10:57:54 -0600

    TASKS FILTERED BY CATEGORY

--- app/src/main/java/com/kikemaya/todoapp/MainActivity.kt
+++ app/src/main/java/com/kikemaya/todoapp/MainActivity.kt
@@ -85,7 +85,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initUI() {
-        categoriesAdapter = CategoriesAdapter(categories)
+        categoriesAdapter = CategoriesAdapter(categories) { position -> updateCategories(position) }
         rvCategories.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvCategories.adapter = categoriesAdapter
@@ -100,7 +100,16 @@ class MainActivity : AppCompatActivity() {
         updateTasks()
     }
 
+    private fun updateCategories(position: Int) {
+        categories[position].isSelected = !categories[position].isSelected
+        categoriesAdapter.notifyItemChanged(position)
+        updateTasks()
+    }
+
     private fun updateTasks() {
+        val selectedCategories: List<TaskCategory> = categories.filter { it.isSelected }
+        val newTasks = tasks.filter { selectedCategories.contains(it.category) }
+        tasksAdapter.tasks = newTasks
         tasksAdapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/kikemaya/todoapp/TasksAdapter.kt
+++ app/src/main/java/com/kikemaya/todoapp/TasksAdapter.kt
@@ -4,7 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class TasksAdapter(private val tasks: List<Task>, private val onTaskSelected: (Int) -> Unit) :
+class TasksAdapter(var tasks: List<Task>, private val onTaskSelected: (Int) -> Unit) :
     RecyclerView.Adapter<TasksViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
         val view =
