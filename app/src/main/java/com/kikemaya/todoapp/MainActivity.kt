876310c6b04bcebc6ebfe4c4c4d40485fea95889
kikemaya <enrique.mg.cd@gmail.com>
2023-06-27 21:36:06 -0600

    DIALOG VIEW

--- app/src/main/java/com/kikemaya/todoapp/MainActivity.kt
+++ app/src/main/java/com/kikemaya/todoapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.kikemaya.todoapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,23 +29,36 @@ class MainActivity : AppCompatActivity() {
     private lateinit var rvTasks: RecyclerView
     private lateinit var tasksAdapter: TasksAdapter
 
+    private lateinit var favAddTask: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initComponent()
         initUI()
+        initListeners()
     }
 
+    private fun initListeners() {
+        favAddTask.setOnClickListener { showDialog() }
+    }
+
+    private fun showDialog() {
+        var dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_task)
+        dialog.show()
+    }
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
         rvTasks = findViewById(R.id.rvTasks)
+
+        favAddTask = findViewById(R.id.favAddTask)
     }
 
     private fun initUI() {
         categoriesAdapter = CategoriesAdapter(categories)
-        rvCategories.layoutManager =
-            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvCategories.adapter = categoriesAdapter
 
         tasksAdapter = TasksAdapter(tasks)
